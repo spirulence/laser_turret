459ede8576165a058c511e98fcbbe2170914ad8c
Cameron Seebach <pannewb@gmail.com>
2019-09-29 16:15:44 -0700

    entities to separate files, game ends

---
+++ entities/gameendgraphic.py
@@ -0,0 +1,18 @@
+import pyglet
+
+
+class GameEndGraphic(object):
+    def __init__(self, image_path):
+        sprite = pyglet.sprite.Sprite(img=pyglet.resource.image(image_path))
+        sprite.image.anchor_x = sprite.image.width / 2
+        sprite.image.anchor_y = sprite.image.height / 2
+        sprite.x = 1920 / 2
+        sprite.y = 1080 / 2
+
+        self.sprite = sprite
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.sprite.draw()
